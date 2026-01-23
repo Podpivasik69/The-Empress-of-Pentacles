5914e5493c3b4bbf3a9db87469bc9cf166f08249
Podpivasik69 <mixail.novikov.2008@mail.ru>
2026-01-24 00:26:07 +0300

    полностью готовая версия

--- utils.py
+++ utils.py
@@ -3,20 +3,14 @@ import os
 
 
 def resource_path(relative_path):
-    """
-    Get absolute path to resource, works for dev and for PyInstaller.
-    PyInstaller создает временную папку в _MEIPASS.
-    """
     try:
-        # PyInstaller создает временную папку в _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
 
     full_path = os.path.join(base_path, relative_path)
 
-    # Для отладки
     if not os.path.exists(full_path) and not getattr(sys, 'frozen', False):
-        print(f"WARNING: Resource not found: {full_path}")
+        print(f"not found {full_path}")
 
     return full_path
